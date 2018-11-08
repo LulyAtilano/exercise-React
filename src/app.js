a636a3ce4845b1a5937db0ac82b1cfb533258c2d
latilanoh@gmail.com <latilanoh@gmail.com>
2018-11-07 23:25:43 -0600

    practice nest components

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+    render() {
+        return ( 
+            <div>
+                <Header/>
+                <Action/>
+                <Options/>
+                <AddOption/>
+            </div>
+        );
+    }
+};
+
 class Header extends React.Component {
     render() {
         return (
@@ -13,7 +26,7 @@ class Action extends React.Component {
     render() {
         return (
             <div>
-                <button> what should I do?</button>
+                <button> What should I do?</button>
             </div>
         )
     }
@@ -24,10 +37,22 @@ class Options extends React.Component {
         return (
             <div>
                 <ol> 
-                    <li> Options are here </li>
+                    <Option/>
+                    <Option/>
+                    <Option/>
                 </ol>
             </div>
         );
+    }
+}
+
+class Option extends React.Component {
+    render() {
+        return (
+            <li>
+                <span> Option component here </span>
+            </li>
+        )
     }
 }
 
@@ -41,13 +66,5 @@ class AddOption extends React.Component {
     }
 }
 
-const jsx = ( 
-    <div> 
-        <Header/>
-        <Action />
-        <Options />
-        <AddOption/>
-    </div>
-);
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
