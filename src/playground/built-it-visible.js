3c72a2c81d06e2bdf1a73af6758093bf8418fec0
latilanoh@gmail.com <latilanoh@gmail.com>
2018-11-12 23:40:29 -0600

    build it adding state to VisibilityToggle component

--- src/playground/built-it-visible.js
+++ src/playground/built-it-visible.js
@@ -1,4 +1,42 @@
-let visibility = false;
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            visibility: false
+        };
+
+        this.handleToggle = this.handleToggle.bind(this);
+    }
+
+    handleToggle() {
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            }
+        });
+    }
+
+    render() {
+        return ( 
+            <div>
+                <h1> Visibility Toggle </h1>
+                <button onClick={this.handleToggle}>
+                    {this.state.visibility ? 'Hide details' : 'Show details'}
+                </button>
+                {this.state.visibility && (
+                    <div>
+                        <p> Hey, these are some details you can now see!!</p>
+                    </div>
+                )}
+            </div>
+        );
+    };
+}
+
+ReactDOM.render(<VisibilityToggle/>, document.getElementById('app'));
+
+/*let visibility = false;
 
 const toggleVisibility  = () => {
     visibility = !visibility;
@@ -23,4 +61,4 @@ const render = () => {
     ReactDOM.render(jsx, document.getElementById('app'));
 }
 
-render();
+render();*/
